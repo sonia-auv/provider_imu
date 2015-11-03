413949728320d8c6ddf28a94217e9fa1a0a2b08d
Antoine Dozois <dozois.a@gmail.com>
2015-11-03 16:49:04 -0500

    Adding file for the euler convertion node

---
+++ src/provider_imu/euler_node.h
@@ -0,0 +1,27 @@
+/**
+ * \file	euler_node.cc
+ * \author	Antoine Dozois <dozois.a@gmail.com>
+ * \date	03/11/2015
+ * \copyright Copyright (c) 2015 S.O.N.I.A. All rights reserved.
+ * \section LICENSE
+ *
+ * This file is part of S.O.N.I.A. software.
+ *
+ * S.O.N.I.A. software is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * S.O.N.I.A. software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with S.O.N.I.A. software. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef PROVIDER_IMU_EULER_NODE_H
+#define PROVIDER_IMU_EULER_NODE_H
+
+#endif //PROVIDER_IMU_EULER_NODE_H
