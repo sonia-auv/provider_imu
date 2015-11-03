05ce75d6faf8b0c9bdb10488e889e0e7c236118d
Antoine Dozois <dozois.a@gmail.com>
2015-11-03 13:56:20 -0500

    Added the output for the firmware

--- src/provider_imu/get_id.cc
+++ src/provider_imu/get_id.cc
@@ -99,6 +99,10 @@ int main(int argc, char **argv) {
   if (verbose) fprintf(stdout, "IMU Device at port %s has ID: ", argv[1]);
   fprintf(stdout, "%s\n", id.c_str());
 
+  std::string firmware = imu.getFirmware(imu.CMD_FIRMWARE_VERSION);
+
+  std::cout << firmware << std::endl;
+
   try {
     imu.closePort();
   } catch (microstrain_3dmgx2_imu::Exception &e) {
